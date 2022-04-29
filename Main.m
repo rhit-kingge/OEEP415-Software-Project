a28424fce07c6bcea8a90271f43e515bf6a0cab5
kingge <kingge@rose-hulman.edu>
2022-04-29 10:30:36 -0400

    Intermediate state, user selects edge orientation then an image will pop up

--- Main.m
+++ Main.m
@@ -3,3 +3,4 @@ clc; clear all;
 
 raw = imread('MTFReference.jpg');
 interface = UserInterfaceC(raw)
+disp(interface.ROI)
--- UserInterfaceC.m
+++ UserInterfaceC.m
@@ -9,29 +9,48 @@ classdef UserInterfaceC
     
     methods
         function obj = UserInterfaceC(image)
+
+%             imshow('reference image.jpg')
+%             p = impixel();
+%             ROI = p;
+
             
-            fig = uifigure('Name','Original Image');
-            fig.Position = [1 1 1200 700];
+            fig = uifigure('Name','Select Edge Orientation');
+            fig.Position = [1 1 500 100];
 
             %Creates button on Gui
-            btn1 = uibutton(fig, 'state');
             btn1X = 75; btn1Y = 50; btnWidth = 100; btnHeight = 22;
-            btn1.Position = [btn1X btn1Y btnWidth btnHeight];
+            btn1Position = [btn1X btn1Y btnWidth btnHeight];
+            btn1 = uibutton(fig, 'push', 'Position', btn1Position, 'ButtonPushedFcn', @(btn1, event) horizontalEdge);
             btn1.Text = 'Horizontal Edge';
 
-            btn2 = uibutton(fig, 'state');
             btn1X = 275;
-            btn2.Position = [btn1X btn1Y btnWidth btnHeight];
+            btn2Position = [btn1X btn1Y btnWidth btnHeight];
+            btn2 = uibutton(fig, 'push', 'Position', btn2Position, 'ButtonPushedFcn', @(btn2, event) verticalEdge);
             btn2.Text = 'Verical Edge';
 
 
-            btn3X = 175; btn3Y = 20;
-            btn3Position = [btn3X btn3Y btnWidth btnHeight];
-            btn3 = uibutton(fig, 'push', 'Position', btn3Position, 'ButtonPushedFcn', @(btn3, event) submitData);
-            btn3.Text = 'Submit';
+            function horizontalEdge
+               disp('Horizontal Button Pressed')         
+            end
 
-            im = uiimage(fig, 'Position', [1 100 1200 600]);
-            im.ImageSource = image;
+            function verticalEdge
+               disp('Vertical Button Pressed')         
+           end
+
+%             btn3X = 175; btn3Y = 20;
+%             btn3Position = [btn3X btn3Y btnWidth btnHeight];
+%             btn3 = uibutton(fig, 'push', 'Position', btn3Position, 'ButtonPushedFcn', @(btn3, event) submitData);
+%             btn3.Text = 'Submit';
+
+%             im = uiimage(fig, 'Position', [1 100 1200 600]);
+%             im.ImageSource = image;
+
+%             function submitData
+%                 disp('Submit Button Pressed')
+%                 a = false;
+%                 
+%             end
 
 
 

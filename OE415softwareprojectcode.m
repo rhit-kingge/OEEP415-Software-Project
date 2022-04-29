916ceaa4630ccd8ec77d6dfe43220928b46b1b14
shenj6 <shenj6@rose-hulman.edu>
2022-04-29 09:33:52 -0400

    resized window

--- OE415softwareprojectcode.m
+++ OE415softwareprojectcode.m
@@ -1,17 +1,19 @@
 clc
-clear all
+clear variables
+close all
 
 %read image and convert to grey
 raw=imread("reference image.jpg");
 img=rgb2gray(raw);
 
 %Creates Gui
-fig = uifigure('Name','My figure');
-pnl = uipanel(fig);
+fig = uifigure('Name','Select the region of interest');
+fig.Position = [1 1 1200 800];
+pnl = uipanel(fig, 'Position', [1 1 1200 800]);
 %Creates button on Gui
-btn = uibutton(pnl,'state','Text','Select an Edge');
+%btn = uibutton(pnl,'state','Text','Select an Edge');
 
 %Creates image spot and then loads image onto Gui
-ima = uiimage(pnl);
+ima = uiimage(pnl, 'Position', [1 1 1200 800]);
 ima.ImageSource = 'reference image.jpg';
 %try img instead of reference image on line 16 to avoid hard coding

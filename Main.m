b16a5e288e205749c754c260ef0b8cc9af22f90a
kingge <kingge@rose-hulman.edu>
2022-05-05 12:15:26 -0400

    Source control

--- Main.m
+++ Main.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 clc 
 clear variables 
 close all
@@ -66,87 +65,6 @@ image(croppedImage, 'XData', [1 size(centroids, 2)], 'YData', [min(centroids) ma
 xArray = 1:size(croppedImage, 1);
 plot(centroids, xArray);
 
-
-
-%% Functions (must be put at the end)
-function [] = pb_call(varargin)
-        % Callback for the pushbutton.
-        errorWindow = varargin{3};  
-        close all;
-end
-
-
-
-
-=======
-clc 
-clear variables 
-close all
-
-%% Read in image, crop ROI, and convert to gray scale
-raw = imread('reference image.jpg');
-interface = UserInterfaceC(raw);
-roi = interface.ROI;
-croppedImage = im2gray(raw(roi(2) + (0:roi(4)), roi(1) + (0:roi(3))));
-imshow(croppedImage);
-
-%% Check if selected ROI contains an edge
-if checkIfEdge(croppedImage, interface.EdgeOrientation)
-    disp('The selected region of interest does contain an edge')
-else
-    errorWindow.fh = figure('units','pixels',...
-                'position',[300 400 340 200],...
-                'menubar','none',...
-                'name','Error',...
-                'NumberTitle','off',...
-                'resize','off');
-    errorWindow.tx = uicontrol('style','text',...
-                 'unit','pix',...
-                 'position',[20 20 300 150],...
-                 'fontsize',16,...
-                 'string','The region you selected contains no edge. Please restart the program and select a new edge.');
-    errorWindow.pb = uicontrol('style','push',...
-                 'unit','pix',...
-                 'position',[20 20 260 30],...
-                 'string','Close');
-    set(errorWindow.pb,'callback',{@pb_call,errorWindow})  % Set the callback for pushbutton.   
-end
-
-%% First estimation of edge location
-
-
-[centroids, isValid] = findCentroid(croppedImage, interface.EdgeOrientation);
-disp("isValid and Centroids")
-disp(centroids);disp("isValid");disp(isValid);
-if isValid == true
-
-else
-    errorWindow.fh = figure('units','pixels',...
-        'position',[300 400 340 200],...
-        'menubar','none',...
-        'name','Error',...
-        'NumberTitle','off',...
-        'resize','off');
-    errorWindow.tx = uicontrol('style','text',...
-        'unit','pix',...
-        'position',[20 20 300 150],...
-        'fontsize',16,...
-        'string','The edge is too close to the rim of ROI. Please restart the program and select a new ROI.');
-    errorWindow.pb = uicontrol('style','push',...
-        'unit','pix',...
-        'position',[20 20 260 30],...
-        'string','Close');
-    set(errorWindow.pb,'callback',{@pb_call,errorWindow})  % Set the callback for pushbutton.
-end
-
-
-
-
-image(croppedImage, 'XData', [1 size(centroids, 2)], 'YData', [min(centroids) max(centroids)]);
-hold on
-xArray = 1:size(croppedImage, 1);
-plot(centroids);
-
 sz = size(centroids);
 
 gradient = polyfit((1:sz),centroids(1:sz,1),1);
@@ -160,5 +78,3 @@ end
 
 
 
-
->>>>>>> fbf900cae58928727832384c1540a871ba141da9
--- findCentroid.m
+++ findCentroid.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [centroids, isValid] = findCentroid(croppedImage, orientation)
 
 
@@ -75,56 +74,4 @@ else
         end
         centroids(column) = c;
     end  
-=======
-function [centroids, isValid] = findCentroid(croppedImage, orientation)
-
-
-
-R = size(croppedImage, 1);
-P = size(croppedImage, 2);
-isValid = true;
-
-if strcmp(orientation, 'Vertical')
-    centroids = zeros(R, 1);
-    for r = 1:R
-        values = double(croppedImage(r,:));
-        hammingVector = values;
-        for i = 1:P
-            hammingVector(i) = 0.54 +0.46 * cos(2*pi*(i-double(P)/2)/(P));
-        end
-        values = hammingVector.*values;
-        numerator = double(0);
-        denominator = double(0);
-        for p = 1:P-1
-            numerator = numerator + p*double(values(1, p+1) - values(1, p));
-            denominator = denominator + double(values(1, p+1) - values(1, p));
-        end
-        c = numerator/denominator;
-        if c <= 2 || P - c <= 2
-            %isValid = false;
-        end
-        centroids(r) = c;
-    end
-else
-    centroids = zeros(P, 1);
-    for p = 1:P
-        values = double(croppedImage(:,p));
-        hammingVector = values;
-        for i = 1:R
-            hammingVector(i) = 0.54 +0.46 * cos(2*pi*(i-double(R)/2)/R);
-        end
-        values = hammingVector.*values;
-        numerator = double(0);
-        denominator = double(0);
-        for r = 1:R-1
-            numerator = numerator + r*double(values(r+1, 1) - values(r, 1));
-            denominator = denominator + double(values(r+1, 1) - values(r, 1));
-        end
-        c = numerator/denominator;
-        if c <= 2 || P - c <= 2
-            %isValid = false;
-        end
-        centroids(p) = c;
-    end  
->>>>>>> fbf900cae58928727832384c1540a871ba141da9
 end

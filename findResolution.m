b87e49652962fee8a1363d36a7a566573d208aaf
brelagc1 <brelagc1@137.112.237.80>
2022-05-20 09:56:34 -0400

    Put a note telling how to do resolution for images taken in class.

--- findResolution.m
+++ findResolution.m
@@ -9,3 +9,5 @@ x = (1:size(MTF, 2))./size(croppedImage, 2)/(3.9E-3);
 
 end
 
+%Edit for 5.2*10^-3 mm * magnification when using images that were taken
+%using cameras in class instead of the 3.9*10^-3 mm.
